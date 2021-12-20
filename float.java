c95ee78a6845253e67f1f15938553b37f07d0f8f
sheethalym <sheethal.xworkz@gmail.com>
2021-12-20 21:42:43 +0530

    adding float program

---
+++ float.java
@@ -0,0 +1,17 @@
+class Float{
+ public static void main(String[]args){
+  float []kd={3.2f,2.3f,1.93f,2.88f,2.45f,2.21f,1.91f,2.23f,4.3f,2.51f};
+  System.out.println(kd[0]);
+ System.out.println(kd[1]);
+ System.out.println(kd[2]);
+ System.out.println(kd[3]);
+ System.out.println(kd[4]);
+ System.out.println(kd[5]);
+ System.out.println(kd[6]);
+ System.out.println(kd[7]);
+ System.out.println(kd[8]);
+ System.out.println(kd[9]);
+
+ }
+
+}
